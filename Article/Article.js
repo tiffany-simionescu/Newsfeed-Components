97f24a496e0ab427a222c93064456d7d12c210f2
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2019-09-09 11:53:22 -0400

    Finished adding articles and fixed button

--- Article/Article.js
+++ Article/Article.js
@@ -85,6 +85,24 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: "My Doggo Ispum Life",
+    date: "Sept 1st, 2019",
+    firstParagraph: `Doggo ipsum extremely cuuuuuute smol wow such tempt, stop it fren. Shooberino noodle horse shooberino I am bekom fat most angery pupper I have ever seen doggorino, you are doing me a frighten big ol pupper he made many woofs long bois. floofs stop it fren. Dat tungg tho very jealous pupper puggo snoot, the neighborhood pupper maximum borkdrive. Smol floofs long bois smol long doggo, floofs heckin good boys. Blep dat tungg tho porgo heckin good boys thicc bork, borkf clouds woofer. very taste wow you are doing me the shock doggo. Doing me a frighten shoober pupperino boofers what a nice floof snoot, the neighborhood pupper boof very taste wow. Maximum borkdrive porgo shoob shibe puggorino, floofs boofers blep.`,
+
+    secondParagraph: `Very hand that feed shibe smol wow very biscit he made many woofs yapper pupperino, shibe noodle horse boofers.  Pupper long woofer waggy wags the neighborhood pupper very good spot shoob, many pats doggo borkdrive h*ck. ruff stop it fren very taste wow. You are doing me the shock long bois what a nice floof length boy woofer yapper, floofs mlem you are doing me the shock doggorino. many pats. Aqua doggo woofer you are doing me a frighten shibe lotsa pats blop, big ol maximum borkdrive heck woofer. Ur givin me a spook blep woofer corgo extremely cuuuuuute, ruff bork.`,
+
+    thirdParagraph: `Porgo adorable doggo woofer much ruin diet you are doing me a frighten h*ck adorable doggo mlem, sub woofer he made many woofs boof you are doin me a concern puggo. Bork extremely cuuuuuute big ol much ruin diet wow very biscit clouds heckin, thicc long bois wow very biscit mlem heckin. Adorable doggo shibe bork heckin good boys, lotsa pats blop. Snoot very good spot vvv ur givin me a spook, long woofer you are doing me a frighten. He made many woofs heck smol, porgo.`
+  },
+  {
+    title: "My Cat Ispum Life",
+    date: "Sept 2nd, 2019",
+    firstParagraph: `Attack feet behind the couch destroy couch flop over give attitude hide when guests come over hopped up on goofballs hunt anything that moves all of a sudden go crazy shake treat  need to chase tail, bag stretch intently stare at the same spot swat at dog make muffins lick butt  intrigued by the shower chew ipad power cord  inspect anything brought into the house  claw drapes chase imaginary bugs  stand in front of the computer screen , behind the couch shake treat  make muffins give attitude chase imaginary bugs hide when guests come over flop over chew ipad power cord  destroy couch.`,
+
+    secondParagraph: `Give attitude behind the couch destroy couch inspect anything brought into the house intently stare at the same spot hopped up on goofballs hunt anything that moves hide when guests come over make muffins lick butt all of a sudden go crazy, attack feet  claw drapes shake treat  swat at dog chew ipad power cord   stand in front of the computer screen   intrigued by the shower need to chase tail chase imaginary bugs, flop over bag stretch destroy couch lick butt hunt anything that moves  claw drapes hopped up on goofballs hide when guests come over flop over.`,
+
+    thirdParagraph: `Bag stretch lick butt  stand in front of the computer screen  intently stare at the same spot chase imaginary bugs  claw drapes shake treat , behind the couch all of a sudden go crazy destroy couch give attitude hopped up on goofballs. Hide when guests come over chase imaginary bugs  stand in front of the computer screen  inspect anything brought into the house make muffins  intrigued by the shower destroy couch swat at dog  claw drapes flop over`
   }
 ];
 
@@ -159,25 +177,27 @@ function articleComponent(
 
   myButton.addEventListener("click", e => {
     articleSection.classList.add("article-open");
-    myButton.classList.toggle("article-open");
-    myButton.classList.toggle("article");
+    articleSection.classList.add("article");
+    articleSection.classList.add("close");
+    myButton.classList.toggle("article-open close");
+    // myButton.classList.toggle("article");
   });
 
   return articleSection;
 }
 
-let newArticleComponent = data.map(arrayItem => {
+let newArticleComponent = data.map(data => {
   let myArticle = articleComponent(
-    arrayItem.title,
-    arrayItem.date,
-    arrayItem.firstParagraph,
-    arrayItem.secondParagraph,
-    arrayItem.thirdParagraph
+    data.title,
+    data.date,
+    data.firstParagraph,
+    data.secondParagraph,
+    data.thirdParagraph
   );
 
   return myArticle;
 });
 
-newArticleComponent.forEach(items => {
-  articles.appendChild(items);
+newArticleComponent.forEach(dataItems => {
+  articles.appendChild(dataItems);
 });
